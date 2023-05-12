0338a68167d0c3737aa7ec776e102e9b46ae0161
TsheringWangchuk <12190099.gcit@rub.edu.bt>
2023-05-12 13:23:10 +0600

    update

--- Druk-E_Bird-BackEnd/app.js
+++ Druk-E_Bird-BackEnd/app.js
@@ -2,7 +2,6 @@ const express = require("express")
 const path = require("path")
 const app = express()
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
 
 const userRouter = require('./Routes/userRoutes')
 const ForgetPasswordRouter = require('./Routes/forgotPasswordRoutes')
