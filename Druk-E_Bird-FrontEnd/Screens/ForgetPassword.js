f07b1193a414bfc1f5da1dbdc79e01bee8f93bb9
TsheringWangchuk <12190099.gcit@rub.edu.bt>
2023-05-12 22:31:51 +0600

    update

--- Druk-E_Bird-FrontEnd/Screens/ForgetPassword.js
+++ Druk-E_Bird-FrontEnd/Screens/ForgetPassword.js
@@ -22,6 +22,7 @@ const ForgetPassword = () => {
         if (res.data.status === 201) {
           let message = typeof res.response !== 'undefined' ? res.response.data.message : res.message;
           ToastAndroid.show(message, ToastAndroid.SHORT);
+          console.log(message)
         }
       })
       .catch(err => {
