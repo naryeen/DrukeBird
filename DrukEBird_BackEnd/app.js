d9005567a7c07ac779a0a21b221ff799c471d289
12190043 <12190043.gcit@rub.edu.bt>
2023-05-15 18:05:37 +0600

    updates

--- DrukEBird_BackEnd/app.js
+++ DrukEBird_BackEnd/app.js
@@ -1,7 +1,11 @@
 const express = require("express")
+const cors = require('cors');
+
 const app = express()
 const cookieParser = require('cookie-parser')
 app.use(cookieParser())
+// Enable CORS for all routes
+app.use(cors());
 
 app.use(express.json())
 
