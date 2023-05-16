cfe5f44d01ca0c309171dad9bfd67ef79c7895ff
TsheringWangchuk <12190099.gcit@rub.edu.bt>
2023-05-16 13:04:28 +0600

    update

--- Druk-E_Bird-BackEnd/server.js
+++ Druk-E_Bird-BackEnd/server.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config({path: './config.env'})
 const app = require('./app')
+const cors = require('cors');
 
 const DB = process.env.DATABASE.replace('PASSWORD',
     process.env.DATABASE_PASSWORD,
@@ -10,6 +11,8 @@ mongoose.set("strictQuery", false);
 mongoose.connect(DB).then((con)=>{
     console.log('DB connection succesful')
 }).catch(error =>console.log(error));
+
+app.use(cors())
 
 const port=4001
 app.listen(port, ()=>{
