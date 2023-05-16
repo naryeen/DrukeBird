cae31ce6019c57f74a61fc8e55aed91f38a79310
TsheringWangchuk <12190099.gcit@rub.edu.bt>
2023-05-16 10:02:10 +0600

    update

--- Druk-E_Bird-BackEnd/Controllers/userController.js
+++ Druk-E_Bird-BackEnd/Controllers/userController.js
@@ -2,8 +2,8 @@ const User = require('../Models/userModels')
 
 exports.getAllUsers = async (req, res, next) => {
     try {
-        const user = await User.find()
-        res.status(200).json({data:user, status: 'success'})
+        const users = await User.find()
+        res.status(200).json({data:users, status: 'success'})
     }catch (err) {
         res.status(500).json({ error: err.message});
     }
