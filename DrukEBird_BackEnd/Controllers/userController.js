fb2f829e00a68e0860c4ffdc4473aa5962402923
12190043 <12190043.gcit@rub.edu.bt>
2023-05-17 14:16:18 +0600

    updates

--- DrukEBird_BackEnd/Controllers/userController.js
+++ DrukEBird_BackEnd/Controllers/userController.js
@@ -2,29 +2,29 @@ const User = require("./../Models/userModels");
 const AppError = require("../utils/appError");
 const multer = require("multer");
 
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./assets/Users");
-  },
-  filename: (req, file, cb) => {
-    // var obj = JSON.parse(req.cookies.token)
-    const ext = file.mimetype.split("/")[1];
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  },
-});
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "./assets/Users");
+//   },
+//   filename: (req, file, cb) => {
+//     // var obj = JSON.parse(req.cookies.token)
+//     const ext = file.mimetype.split("/")[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   },
+// });
 
-const multerFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("image")) {
-    cb(null, true);
-  } else {
-    cb(new AppError("Not an image! Please upload only images.", 400), false);
-  }
-};
-const upload = multer({
-  storage: multerStorage,
-  fileFilter: multerFilter,
-});
-exports.uploadUserPhoto = upload.single("photo");
+// const multerFilter = (req, file, cb) => {
+//   if (file.mimetype.startsWith("image")) {
+//     cb(null, true);
+//   } else {
+//     cb(new AppError("Not an image! Please upload only images.", 400), false);
+//   }
+// };
+// const upload = multer({
+//   storage: multerStorage,
+//   fileFilter: multerFilter,
+// });
+// exports.uploadUserPhoto = upload.single("photo");
 
 exports.getAllUsers = async (req, res, next) => {
   try {
