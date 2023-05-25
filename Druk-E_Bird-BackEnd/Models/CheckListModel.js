cab7b818c99bfb4360c71e8ad81e3bca652f247d
TsheringWangchuk <12190099.gcit@rub.edu.bt>
2023-05-25 11:40:18 +0600

    update

--- Druk-E_Bird-BackEnd/Models/CheckListModel.js
+++ Druk-E_Bird-BackEnd/Models/CheckListModel.js
@@ -1,21 +1,21 @@
-const mongoose = require ('mongoose')
+const mongoose = require('mongoose');
 const moment = require('moment');
 
 const currentLocation = new mongoose.Schema({
-    latitude:{
-        type:Number,
-        required: [true, 'latitude' ],
-        default:0
-    },
+  latitude: {
+    type: Number,
+    required: [true, 'latitude'],
+    default: 0,
+  },
+  longitude: {
+    type: Number,
+    required: [true, 'longitude'],
+    default: 0,
+  },
+});
 
-    longitude:{
-        type:Number,
-        required: [true, 'longtitude' ],
-        default:0
-    }
-})
-
-const EndpointLocation = [new mongoose.Schema({
+const EndpointLocation = [
+  new mongoose.Schema({
     dzongkhag: {
       type: String,
       default: "null",
@@ -28,60 +28,51 @@ const EndpointLocation = [new mongoose.Schema({
       type: String,
       default: "null",
     },
-  })];
-   
+  }),
+];
 
 const detailSchema = new mongoose.Schema({
-    selectedDate: {
-        type: String,
-        required: [true, 'Date' ],
-        default: moment().format('YYYY-MM-DD'),
-    },
+  selectedDate: {
+    type: String,
+    required: [true, 'Date'],
+    default: moment().format('YYYY-MM-DD'),
+  },
+  selectedTime: {
+    type: String,
+    required: [true, 'Time'],
+    default: moment().format('HH:mm:ss'),
+  },
+  observer: {
+    type: String,
+    required: [true, 'observer'],
+    default: "null",
+  },
+  currentLocation: currentLocation,
+  count: {
+    type: Number,
+    required: [true, 'Count'],
+    default: 0,
+  },
+  photo: {
+    type: String,
+    default: "null",
+  },
+  EndpointLocation: EndpointLocation,
+});
 
-    selectedTime: {
-        type: String,
-        required: [true, 'Time'],
-        default: moment().format('HH:mm:ss')
-    },
-
-    observer: {
-        type: String,
-        required: [true, 'observer'],
-        default: "null"
-    },
-    
-    currentLocation: currentLocation,
-   
-    count: {
-        type: Number,
-        required: [true, 'Count'],
-        default: 0
-    },
-    photo:{
-        type: String,
-        default: "null"
-    },
-    
-    EndpointLoaction: EndpointLocation
-
-})
-
-// mongooose middleware
 const BirdsSchema = new mongoose.Schema({
-    CheckListName: {
-        type: String,
-        required: [true, 'CheckListName'],
-        default:"null"
-    },
-    BirdName: {
-        type: String,
-        required: [true, 'BirdName'],
-        default:"null"
-    },
-    StartbirdingData: [detailSchema]
-})
+  CheckListName: {
+    type: String,
+    required: [true, 'CheckListName'],
+    default: "null",
+  },
+  BirdName: {
+    type: String,
+    required: [true, 'BirdName'],
+    default: "null",
+  },
+  StartbirdingData: [detailSchema],
+});
 
-const checkList = mongoose.model('checkList', BirdsSchema )
-module.exports = checkList
-
-
+const checkList = mongoose.model('checkList', BirdsSchema);
+module.exports = checkList;
